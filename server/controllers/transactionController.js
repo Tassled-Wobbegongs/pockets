930ed40cd5b24bb6a7cbb697614c88e4ef47e619
Timothy <timothysin707@gmail.com>
2021-10-16 16:29:58 -0700

    finished database queries and server logic

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -11,7 +11,7 @@ transactionController.addTransaction = (req, res, next) => {
     const addTransQuery = `INSERT INTO public.transactions (name, amount, date, category_id) VALUES ($1, $2, $3, $4) RETURNING *`; 
     const values = [req.body.name, req.body.amount, req.body.date, req.body.category_id]
     // console.log(addTransQuery);
-    // console.log(req.body);
+    console.log(typeof req.body.amount);
     // console.log(values);
 
     db.query(addTransQuery, values)
@@ -21,15 +21,59 @@ transactionController.addTransaction = (req, res, next) => {
             return next();
         })
         .catch( err => {
-            console.log('query error');
+            console.log('query error', err);
             return next(err);
         });
 };
 
+//MIDDLEWARE FOR RETRIEVING TRANSACTION DATA FOR FRONTEND DISPLAY
+transactionController.getTransaction = (req, res, next) => {
+    const getTransQuery = `SELECT transactions.*, categories.category as category 
+    FROM transactions 
+    LEFT OUTER JOIN categories ON categories._id = transactions.category_id`;
+    db.query(getTransQuery)
+        .then(data => {
+            // console.log(data.rows);
+            res.locals.data = data.rows;
+            return next();
+        })
+        .catch(err => {
+            console.log('get query error', err);
+            return next(err);
+        });
+};
 
+//MIDDLEWARE FOR DELETING A TRANSACTION
+transactionController.deleteTransaction = (req, res, next) => {
+    const deleteQuery = `DELETE FROM transactions WHERE transaction_id=${req.body.id}`;
 
+    //receive transaction_id from the request on req.body.id
+    
+    db.query(deleteQuery)
+        .then(data => {
+            // console.log(data)
+            return next();
+        })
+            .catch(err => {
+            console.log('delete error', err);
+            return next(err);
+        })
+};
 
+//MIDDLEWARE FOR CALCULATING RUNNING TOTAL OF TRANSACTIONS
+transactionController.getTotal = (req, res, next) => {
+    //res.locals.data should have all our transactions
+    const transactions = res.locals.data;
+    let total = 0;
+        
+    transactions.forEach( obj => {
+        total += obj.amount;
+        
+    })
 
+    res.locals.total = total;
+    return next();
+};
 
 
 
